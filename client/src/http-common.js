0a9efda7622e2a995dfe10d740e1f7b46b1f8fd7
rafaelbrazrocha <64224283+rafaelbrazrocha@users.noreply.github.com>
2020-07-17 20:00:49 -0300

    changing server

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'hhttp://danielrocha-desafio-final.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
